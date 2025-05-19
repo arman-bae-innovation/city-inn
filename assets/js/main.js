08e46041162c722c393b876f7eb28726cf202eea
arman9022 <arman.hub.bd@gmail.com>
2025-05-19 14:01:03 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -127,8 +127,8 @@ function updateThemeByTime() {
   const now = new Date();
   const currentMinutes = now.getHours() * 60 + now.getMinutes();
 
-  const darkModeEnd = 13 * 60 + 59;
-  const darkModeStart = 14 * 60 + 0;
+  const darkModeEnd = 14 * 60 + 2;
+  const darkModeStart = 14 * 60 + 3;
 
   const isDarkTime = currentMinutes >= darkModeStart || currentMinutes < darkModeEnd;
 
