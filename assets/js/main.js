a26161bda4e7abb746eb5e9169b8cca4399d4aa2
arman9022 <arman.hub.bd@gmail.com>
2025-05-19 11:26:19 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,39 +125,80 @@ $(document).ready(function(){
 
 
 // LIVE CLOCK START
-function updateClock() {
-  const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
 
-  const ampm = hours >= 12 ? 'PM' : 'AM';
-  const hour12 = hours % 12 || 12;
 
-  const paddedMinutes = minutes.toString().padStart(2, '0');
-  const paddedSeconds = seconds.toString().padStart(2, '0');
+// function updateClock() {
+//   const now = new Date();
+//   const hours = now.getHours();
+//   const minutes = now.getMinutes();
+//   const seconds = now.getSeconds();
+
+//   const ampm = hours >= 12 ? 'PM' : 'AM';
+//   const hour12 = hours % 12 || 12;
+
+//   const paddedMinutes = minutes.toString().padStart(2, '0');
+//   const paddedSeconds = seconds.toString().padStart(2, '0');
   
-  const timeString = `${hour12}:${paddedMinutes}:${paddedSeconds} ${ampm}`;
-  document.getElementById('live-clock').textContent = timeString;
-}
-setInterval(updateClock, 1000);
-updateClock();
+//   const timeString = `${hour12}:${paddedMinutes}:${paddedSeconds} ${ampm}`;
+//   document.getElementById('live-clock').textContent = timeString;
+// }
+// setInterval(updateClock, 1000);
+// updateClock();
+
+
 // LIVE CLOCK END
 
 
 // AUTO THEME CHANGE FOR TIME START 
+
+
+// function autoThemeSwitch() {
+//   const now = new Date();
+//   const hours = now.getHours();
+//   const minutes = now.getMinutes();
+//   const seconds = now.getSeconds();
+
+//   const icon = document.getElementById("themeIcon");
+//   const isDark = document.body.classList.contains("dark-mode");
+
+//   const currentTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+//   if (currentTime === "6:00:00" && isDark) {
+//     document.body.classList.remove("dark-mode");
+//     icon.classList.remove("bi-brightness-high-fill");
+//     icon.classList.add("bi-moon-stars-fill");
+//     localStorage.setItem("PageTheme", JSON.stringify("LIGHT"));
+//     console.log("Switched to LIGHT theme");
+//   }
+
+//   if (currentTime === "11:20:00" && !isDark) {
+//     document.body.classList.add("dark-mode");
+//     icon.classList.remove("bi-moon-stars-fill");
+//     icon.classList.add("bi-brightness-high-fill");
+//     localStorage.setItem("PageTheme", JSON.stringify("DARK"));
+//     console.log("Switched to DARK theme");
+//   }
+// }
+
+// setInterval(() => {
+//   updateClock();
+//   autoThemeSwitch();
+// }, 1000);
+
+
+// AUTO THEME CHANGE FOR TIME END 
+
 function autoThemeSwitch() {
   const now = new Date();
   const hours = now.getHours();
   const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
 
   const icon = document.getElementById("themeIcon");
   const isDark = document.body.classList.contains("dark-mode");
 
-  const currentTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  const currentTime = `${hours}:${minutes}`;
 
-  if (currentTime === "6:00:00" && isDark) {
+  if (currentTime === "6:0" && isDark) {
     document.body.classList.remove("dark-mode");
     icon.classList.remove("bi-brightness-high-fill");
     icon.classList.add("bi-moon-stars-fill");
@@ -165,7 +206,7 @@ function autoThemeSwitch() {
     console.log("Switched to LIGHT theme");
   }
 
-  if (currentTime === "11:20:00" && !isDark) {
+  if (currentTime === "11:28" && !isDark) {
     document.body.classList.add("dark-mode");
     icon.classList.remove("bi-moon-stars-fill");
     icon.classList.add("bi-brightness-high-fill");
@@ -173,10 +214,6 @@ function autoThemeSwitch() {
     console.log("Switched to DARK theme");
   }
 }
-
 setInterval(() => {
-  updateClock();
   autoThemeSwitch();
 }, 1000);
-// AUTO THEME CHANGE FOR TIME END 
-
